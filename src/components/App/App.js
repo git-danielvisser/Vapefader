e3b1f7b1ee77e6bd3b3e288292c1e45026ad4f1e
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-01-07 09:43:57 +0100

    Moved file and added some basic jsx

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Branding from '../Branding/Branding.js';
+import SchemaForm from '../SchemaForm/SchemaForm.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Branding
+        name="Vapefade"
+        tagline="Quit vaping or smoking gradually."
+      />
+      <SchemaForm 
+
+      />
+    </div>
+  );
+}
+
+export default App;
