d259d109cbf2f5a8546e66c82ac17417010a3c86
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-01-11 10:35:31 +0100

    Added component prefix

--- src/components/Branding/Branding.js
+++ src/components/Branding/Branding.js
@@ -1,10 +1,12 @@
+import './Branding.scss';
+
 function Branding(props) {
     return (
-        <div className="branding">
-            <h1 className="branding__name">
+        <div className="c-branding">
+            <h1 className="c-branding__name">
                 {props.name}
             </h1>
-            {props.tagline && <h2 className="branding__tagline">
+            {props.tagline && <h2 className="c-branding__tagline">
                 {props.tagline}
             </h2>}
         </div>
