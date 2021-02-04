dcec7fa19bcf3abdb1d3d6c1c23ee48fb5c6ab4f
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-04 14:10:22 +0100

    Refactored App component
    App now handles the form submit and creation of the schema.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,56 @@
-import './App.css';
-import Branding from '../Branding/Branding.js';
-import SchemaForm from '../SchemaForm/SchemaForm.js';
+import React, { useState } from "react";
+import generateSchema from "../../generateSchema";
+
+import Branding from "../Branding/Branding.js";
+import Schema from "../Schema/Schema.js";
+import SchemaForm from "../SchemaForm/SchemaForm.js";
+
+import { Box, Container, makeStyles, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  formSlide: {
+    background:
+      "linear-gradient(180deg, rgba(245,0,87,1) 0%, rgba(63,81,181,1) 100%)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 90%",
+    padding: "20px 10px",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  let [schemaItems, setSchemaItems] = useState(null);
+
+  const handleFormSubmit = (formData) => {
+    setSchemaItems(generateSchema(formData));
+  };
+
   return (
     <div className="App">
-      <Branding
-        name="Vapefade"
-        tagline="Quit vaping or smoking gradually."
-      />
-      <SchemaForm 
-
-      />
+      <Box p={2} className={classes.formSlide}>
+        <Container maxWidth="sm">
+          <header className="header">
+            <Branding
+              name="Vapefader"
+              tagline="Gradually quit vaping or smoking  with a generated schema."
+            />
+          </header>
+          <Box mt={3}>
+            <Paper>
+              <Box p={5}>
+                <SchemaForm onSubmit={handleFormSubmit} />
+              </Box>
+            </Paper>
+          </Box>
+        </Container>
+      </Box>
+      {schemaItems && (
+        <Box p={2} mt={5}>
+          <Container maxWidth="md">
+            <Schema schemaItems={schemaItems} />
+          </Container>
+        </Box>
+      )}
     </div>
   );
 }
