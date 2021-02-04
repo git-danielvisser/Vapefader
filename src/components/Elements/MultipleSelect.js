3951ca29ebf15379db2303e141f30df57d2548c9
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-04 14:03:01 +0100

    Added multiple select component

---
+++ src/components/Elements/MultipleSelect.js
@@ -0,0 +1,56 @@
+// https://codesandbox.io/s/cwvg4
+
+import { TextField, MenuItem, Checkbox, ListItemText } from "@material-ui/core";
+
+export default function MultipleSelect(props) {
+  const {
+    id,
+    name,
+    label,
+    placeholder,
+    value,
+    error,
+    options,
+    onChange,
+    variant,
+  } = props;
+
+  const handleChange = (e) => {
+    const ordered = [];
+    options.forEach((option) => {
+      if (e.target.value.includes(option.value)) {
+        ordered.push(option.value);
+      }
+    });
+
+    e.target.value = ordered;
+    onChange(e);
+  };
+
+  return (
+    <TextField
+      select
+      SelectProps={{
+        multiple: true,
+        renderValue: (selected) => selected.join(", "),
+      }}
+      id={id}
+      name={name}
+      label={label}
+      placeholder={placeholder}
+      value={value}
+      error={error}
+      helperText={error}
+      onChange={handleChange}
+      variant={variant}
+      fullWidth
+    >
+      {options.map((option) => (
+        <MenuItem alignItems="center" key={option.value} value={option.value}>
+          <Checkbox checked={value.indexOf(option.value) > -1} />
+          <ListItemText primary={option.label} />
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+}
