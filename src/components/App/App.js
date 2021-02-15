8993c6d3575cc01df3b40831d4b12adf833f9e4a
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-15 10:42:42 +0100

    Multiple changes
    Moved generate schema in to webworker. Scrolls to schema on form submit. Changed Branding component props.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,52 +1,68 @@
-import React, { useState } from "react";
-import generateSchema from "../../generateSchema";
+import React, { useState, useRef } from "react";
+import generateSchemaWorker from "../../generateSchema/generateSchema.worker.js";
+import { smoothScrollTo } from "../../helpers.js";
 
 import Branding from "../Branding/Branding.js";
 import Schema from "../Schema/Schema.js";
 import SchemaForm from "../SchemaForm/SchemaForm.js";
 
-import { Box, Container, makeStyles, Paper } from "@material-ui/core";
+import { Box, Container, Paper, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   formSlide: {
     background:
-      "linear-gradient(180deg, rgba(245,0,87,1) 0%, rgba(63,81,181,1) 100%)",
+      "linear-gradient(166deg, rgba(245,0,87,1) 10%, rgba(63,81,181,1) 90%)",
     backgroundRepeat: "no-repeat",
-    backgroundSize: "100% 90%",
-    padding: "20px 10px",
+    backgroundSize: "100% 100%",
+    boxSizing: "border-box",
+    minHeight: "100vh",
   },
 });
 
 function App() {
+  const [schemaItems, setSchemaItems] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const classes = useStyles();
-  let [schemaItems, setSchemaItems] = useState(null);
+  const schemaContainerEle = useRef(null);
 
   const handleFormSubmit = (formData) => {
-    setSchemaItems(generateSchema(formData));
+    setIsLoading(true);
+
+    const worker = new generateSchemaWorker();
+    worker.postMessage(formData);
+    worker.onmessage = (e) => {
+      setSchemaItems(e.data);
+      setIsLoading(false);
+      smoothScrollTo(schemaContainerEle.current, -40);
+    };
   };
 
   return (
     <div className="App">
-      <Box p={2} className={classes.formSlide}>
+      <Box className={classes.formSlide}>
         <Container maxWidth="sm">
           <header className="header">
-            <Branding
-              name="Vapefader"
-              tagline="Gradually quit vaping or smoking  with a generated schema."
-            />
+            <Box p={5}>
+              <Branding
+                brand="Vapefader"
+                tagline="Gradually quit vaping or smoking  with a generated schema."
+              />
+            </Box>
           </header>
-          <Box mt={3}>
+          <Box>
             <Paper>
               <Box p={5}>
-                <SchemaForm onSubmit={handleFormSubmit} />
+                <SchemaForm onSubmit={handleFormSubmit} isLoading={isLoading} />
               </Box>
             </Paper>
           </Box>
         </Container>
       </Box>
+
       {schemaItems && (
         <Box p={2} mt={5}>
-          <Container maxWidth="md">
+          <Container ref={schemaContainerEle} maxWidth="md">
             <Schema schemaItems={schemaItems} />
           </Container>
         </Box>
