3a9259bd202a797dc16f685bc7a84185f82a3dce
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-15 10:49:49 +0100

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -6,21 +6,8 @@ export function getDaysFromTo(dateA, dateB) {
   return Math.round((dateB - dateA) / 86400000); // (dateB - dateA) / 1000 / 60 / 60 / 24
 }
 
-export function datesFromFrequentie(
-  startDate,
-  totalDates,
-  frequentie,
-  offsetStartDate = false
-) {
-  if (offsetStartDate) {
-    startDate.setDate(startDate.getDate() + frequentie);
-  }
-
-  const dates = [];
-  for (let i = 0; i < totalDates; i++) {
-    const date = new Date(startDate);
-    date.setDate(startDate.getDate() + frequentie * i);
-    dates.push(date);
-  }
-  return dates;
+export function smoothScrollTo(element, yOffset) {
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({ top: y, behavior: "smooth" });
 }
+
