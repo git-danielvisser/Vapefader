dc0e956288cb1e7f1bd8491a339948b832115d23
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-15 10:45:58 +0100

    Create LiquidMixPart.js

---
+++ src/components/Schema/LiquidMixes/LiquidMixPart.js
@@ -0,0 +1,34 @@
+import { Box, Chip, Grid, Paper, Typography } from "@material-ui/core";
+import { Opacity, RadioButtonChecked, Timelapse } from "@material-ui/icons";
+
+export default function LiquidInfo({ strength, percentage, ml }) {
+  return (
+    <Paper>
+      <Box p={1}>
+        <Grid container spacing={1} alignItems="center" justify="center">
+          <Grid item>
+            <Chip
+              label={`${strength} mg/ml`}
+              icon={<Opacity />}
+              size="small"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item>
+            <Chip
+              label={`${percentage}%`}
+              icon={percentage === 100 ? <RadioButtonChecked /> : <Timelapse />}
+              size="small"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body2" align="center">
+              {`${ml}ml`}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Paper>
+  );
+}
