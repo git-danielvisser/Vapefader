fdc2386e451f42d0f4faf629348d21f550a4c9ea
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-15 11:10:10 +0100

    Added template strings

--- src/components/Schema/SchemaItem.js
+++ src/components/Schema/SchemaItem.js
@@ -37,28 +37,26 @@ export default function SchemaItem({
         <Box mt={1}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-         
-                  <Grid container spacing={1} justify="center">
-                    {sessions !== undefined && (
-                      <Grid item>
-                        <Chip
-                          label={sessions + " sessions"}
-                          icon={<SmokeFree />}
-                          color={sessions !== 0 ? "secondary" : "default"}
-                        />
-                      </Grid>
-                    )}
-                    {strength !== undefined && (
-                      <Grid item>
-                        <Chip
-                          label={strength + " mg/ml"}
-                          icon={<Opacity />}
-                          color={strength !== 0 ? "primary" : "default"}
-                        />
-                      </Grid>
-                    )}
+              <Grid container spacing={1} justify="center">
+                {sessions !== undefined && (
+                  <Grid item>
+                    <Chip
+                      label={`${sessions} sessions"`}
+                      icon={<SmokeFree />}
+                      color={sessions !== 0 ? "secondary" : "default"}
+                    />
                   </Grid>
-        
+                )}
+                {strength !== undefined && (
+                  <Grid item>
+                    <Chip
+                      label={`${strength} mg/ml"`}
+                      icon={<Opacity />}
+                      color={strength !== 0 ? "primary" : "default"}
+                    />
+                  </Grid>
+                )}
+              </Grid>
             </Grid>
             {message !== undefined && (
               <Grid item xs={12}>
