fa50c706dbafc5b59cd019838f8a0be9e87bd900
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-15 10:43:31 +0100

    Replaced css with material-ui styles

--- src/components/Branding/Branding.js
+++ src/components/Branding/Branding.js
@@ -1,16 +1,31 @@
-import './Branding.scss';
+import { Box, Typography, makeStyles } from "@material-ui/core";
 
-function Branding(props) {
-    return (
-        <div className="c-branding">
-            <h1 className="c-branding__name">
-                {props.name}
-            </h1>
-            {props.tagline && <h2 className="c-branding__tagline">
-                {props.tagline}
-            </h2>}
-        </div>
-    )
+const useStyles = makeStyles({
+  brand: {
+    color: "#fff",
+    fontFamily: "'Sarabun', sans-serif",
+    fontSize: "2.4rem",
+    letterSpacing: "unset",
+    marginBottom: "30px",
+  },
+  tagline: { color: "#fff", fontSize: "1rem" },
+});
+
+function Branding({ brand, tagline }) {
+  const classes = useStyles();
+
+  return (
+    <Box align="center">
+      <Typography variant="h1" className={classes.brand}>
+        {brand}
+      </Typography>
+      {tagline && (
+        <Typography variant="h2" className={classes.tagline}>
+          {tagline}
+        </Typography>
+      )}
+    </Box>
+  );
 }
 
 export default Branding;
