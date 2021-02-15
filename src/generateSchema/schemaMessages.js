e22e19c34f4de9e8f9ee4b0c371a44363b409c1d
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-15 10:49:37 +0100

    Create schemaMessages.js

---
+++ src/generateSchema/schemaMessages.js
@@ -0,0 +1,17 @@
+export const strengthProgressMessages = {
+  start:
+    "We will start by decreasing nicotine strength, good luck and strong wilpower.😄",
+  qaurter: "Good progress, keep going! 💪",
+  half: "Your half way there, keep going! 🐉",
+  threeQaurter: "Almost there, just a bit more.⌛️🏁",
+  end: "You made it! 📈❤️🍆",
+};
+
+export const sessionProgressMessages = {
+  start: "We will start decreasing sessions, good luck and you got this.😄",
+  qaurter: "Your on your way, good progress! 🙏",
+  half: "Half way there! You proved that you can do it! 🐲",
+  threeQaurter: "Almost there, just a bit more.⌛️🏁",
+  end: "You made it! 📈❤️💎🤑",
+};
+
