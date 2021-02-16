0381ff747f6429023b2f7a9930aa54aa1e34890d
git-danielvisser <git-danielvisser@users.noreply.github.com>
2021-02-16 16:23:26 +0100

    Improved visuals
    The liquid mixes table is now easier to scan trough as a user.

--- src/components/Schema/LiquidMixes/LiquidMixPart.js
+++ src/components/Schema/LiquidMixes/LiquidMixPart.js
@@ -1,32 +1,36 @@
 import { Box, Chip, Grid, Paper, Typography } from "@material-ui/core";
-import { Opacity, RadioButtonChecked, Timelapse } from "@material-ui/icons";
+import {
+  Colorize,
+  Opacity,
+  RadioButtonChecked,
+  Timelapse,
+} from "@material-ui/icons";
 
 export default function LiquidInfo(props) {
   const { strength, percentage, ml } = props;
   return (
     <Paper>
       <Box p={1}>
-        <Grid container spacing={1} alignItems="center" justify="center">
+        <Grid container spacing={1} alignItems="baseline">
           <Grid item>
             <Chip
               label={`${strength} mg/ml`}
               icon={<Opacity />}
               size="small"
-              variant="outlined"
+              color="secondary"
             />
           </Grid>
-          <Grid item>
+          <Grid item xs>
             <Chip
               label={`${percentage}%`}
               icon={percentage === 100 ? <RadioButtonChecked /> : <Timelapse />}
               size="small"
+              color="primary"
               variant="outlined"
             />
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body2" align="center">
-              {`${ml}ml`}
-            </Typography>
+          <Grid item>
+            <Typography variant="body2">{`${ml}ml`}</Typography>
           </Grid>
         </Grid>
       </Box>
